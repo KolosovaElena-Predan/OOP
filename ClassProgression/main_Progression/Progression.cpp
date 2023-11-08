ed8f2d78893a3cd210ee81ff231622f5fda01af0
Kolosova Elena <elena.kolosova.04@mail.ru>
2023-11-08 22:46:45 +0900

    Add test

--- ClassProgression/main_Progression/Progression.cpp
+++ ClassProgression/main_Progression/Progression.cpp
@@ -53,21 +53,23 @@
 			return FirstElem + Step * (number - 1);
 
 		else
-			if (Step != 1)
 				return FirstElem * pow(Step, number - 1);
-			//else throw std::exception("Incorrect progression step value");
+
 
 	}
 
 	///поиск суммы первых quantity элементов
 	double Progression::Find_the_sum(int quantity){
-		if (PType == ProgressionType::Arithmetic) //проверка на положительность
+		if (PType == ProgressionType::Arithmetic) 
 			return (2 * FirstElem + Step * (quantity - 1)) / 2.0 * quantity;
 
 		else
-			if (Step != 1)
+			if (Step==1) {
+				return FirstElem* quantity;
+			}
+			else 
 				return (FirstElem * (pow(Step, quantity) - 1)) / (Step - 1);
-			//else throw std::exception("Incorrect progression step value");
+				
 	}
 
 	///преобразование данных класса в строку
@@ -80,8 +82,45 @@
 
 	///проверка правильности выполнения функций Value_n_element и Find_the_sum 
 	void Progression_Test() {
-		Progression P;
+
+		Progression P; 
+		
+		//1 проверка с целыми числами арифметической прогрессии
 		P.set_FirstElem(1);
 		P.set_Step(1);
 		assert(P.Find_the_sum(3)==6);
+		assert(P.Value_n_element(4) == 4);
+
+		//2 проверка с вещественными числами арифметической прогрессии
+		P.set_FirstElem(2.3);
+		P.set_Step(0.93);
+		assert(P.Find_the_sum(5) == 20.8);
+		assert(P.Value_n_element(3) == 4.16);
+
+		//3 проверка с отрицательными вещественными числами арифметической прогрессии
+		P.set_FirstElem(-2.3);
+		P.set_Step(-0.93);
+		assert(P.Find_the_sum(6) == -27.75);
+		assert(P.Value_n_element(7) == -7.88);
+
+		//1 проверка с целыми числами геометрической прогрессии
+		P.set_FirstElem(1);
+		P.set_Step(2);
+		P.set_PType(ProgressionType::Geometric);
+		assert(P.Find_the_sum(3) == 7);
+		assert(P.Value_n_element(4) == 8);
+
+		//2 проверка с вещественными числами геометрической прогрессии
+		P.set_FirstElem(2.3);
+		P.set_Step(0.93);
+		assert(P.Find_the_sum(5) - 9.99881 < 0.00001);
+		assert(P.Value_n_element(3) == 1.98927);
+
+		//3 проверка с отрицательными вещественными числами геометрической прогрессии
+		P.set_FirstElem(-2.3);
+		P.set_Step(-0.93);
+		assert(abs(P.Find_the_sum(6) - ( - 0.42068)) < 0.00001);
+		assert(abs(P.Value_n_element(7) - ( - 1.48807)) < 0.00001);
+
+
 	}
